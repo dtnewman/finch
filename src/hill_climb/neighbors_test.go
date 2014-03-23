31ac49b322f721f9b6c2090e82856bf49453af2f
Daniel Newman <dnewman@fas.harvard.edu>
2014-03-23 16:19:29 -0400

    added test for neighbors

---
+++ src/hill_climb/neighbors_test.go
@@ -0,0 +1,37 @@
+package main
+
+import "testing"
+
+/** Test the function simple_get_neighbors() to ensure that it gives
+	the correct output for the input array {1,2} */
+func Test_simple_get_neighbors(t *testing.T) {
+	two_arrays_equal := true
+	input := []int{1, 2}
+	got := simple_get_neighbors(input)
+	want := make([][]int,0)
+	want = append(want,[]int{2,2})
+	want = append(want,[]int{1,1})
+	want = append(want,[]int{1,3})
+
+	// check that arrays are the same size
+	if len(want) != len(got) {
+		two_arrays_equal = false
+	} else {
+		for i := 0; i < len(want); i++ {
+			// check length of each subarray
+			if len(want[i]) != len(got[i]) {
+				two_arrays_equal = false
+			} else {
+				//now check the content of each subarray
+				for j := 0; j < len(want[i]); j++ {
+					if want[i][j] != got[i][j] {
+						two_arrays_equal = false
+					}
+				}
+			}
+		}
+	}
+	if (!two_arrays_equal) {
+		t.Errorf("simple_get_neighbors(%d) == %d, want %d", input, got, want)
+	}
+}
