7c076117a627d2ab625640d59f4acc6e99656f32
Daniel Newman <dtnewman@gmail.com>
2014-04-02 13:20:24 -0400

    added stochastic beam search

--- src/beam_search/beam_search.go
+++ src/beam_search/beam_search.go
@@ -118,8 +118,92 @@ func beam_search(num_beams int, evaluate func([]int) float64,
 	}
 
 	return population[0], max_fitness
-
 }
+
+// This is my implementation of beam search. It generates a random starting population of size num_beams. 
+// Then, at every step, it looks at the neighbors of all members of the population and then finds the top
+// neighbors to create a new population of size num_beams. This keeps going until the max score is no longer
+// improving at which point it stops and returns the best solution.
+func stochastic_beam_search(num_beams int, evaluate func([]int) float64, 
+				create_random func() []int, get_neighbors func([]int) [][]int)([]int, float64) { 
+	// decalre variables
+	var next_generation_candidates [][]int
+	var neighbors [][]int
+	var max_fitness float64 = math.Inf(-1)
+	var sum_fitnesses float64
+	var rand_float float64
+	var cum_probability float64
+
+	// create a population of num_beams random starting points 
+	population := make([][]int,0)
+	for i:=0; i < num_beams; i++ {
+		population = append(population, create_random())
+	}
+	for {// num_iterations := 0;num_iterations < 100 ; num_iterations++{
+		//fmt.Println(num_iterations)
+		// get all the neighbors for all beams and put them into next_generation_candidates
+		next_generation_candidates = make([][]int,0)
+		for i:=0; i < num_beams; i++ {
+			neighbors = get_neighbors(population[i])
+			for _, value := range neighbors {
+				next_generation_candidates = append(next_generation_candidates, value)
+
+			}
+		}
+
+		// evaluate all the solutions and store values in fitness array
+		fitnesses := make([][]float64,0)
+		fitnesses = append(fitnesses,make([]float64,len(next_generation_candidates)))
+		fitnesses = append(fitnesses,make([]float64,len(next_generation_candidates)))
+
+		for i, _ := range fitnesses[0] {
+			// negate the evaluations, so that it sorts from largest to smallest
+			fitnesses[0][i] = -evaluate(next_generation_candidates[i])
+			fitnesses[1][i] = float64(i)
+		}
+
+		// sort the fitness array by value in the first row, which holds fitness scores
+		fitnesses = qsort_2d(fitnesses,0)
+		
+		// Stop the loop if the highest fitness is not greater than max_fitness
+		if -fitnesses[0][0] <= max_fitness{
+			break
+		} 
+
+
+		// we're gonna pick based on order, with items first getting higher probability. First
+		sum_fitnesses =  float64(len(fitnesses[0])*(len(fitnesses[0])+1))/2.0
+
+		selection_probability := make([]float64,len(fitnesses[0]))
+		cum_probability = 0.0
+
+		for i, _ := range fitnesses[0] {
+			selection_probability[i] = cum_probability+float64(len(fitnesses[0])-i)/sum_fitnesses
+			cum_probability += float64(len(fitnesses[0])-i)/sum_fitnesses
+		}
+
+		// Set max_fitness to (-1) times the first element in the first row of fitnesses
+		// since that indicates the leading value
+		max_fitness = -fitnesses[0][0]
+
+
+		// Now fill up the next generation based on probabilites that are determined from
+		// position
+		for i := 1; i < num_beams; i++ {
+			rand_float = rand.Float64()
+			j := 0
+			for j=0; selection_probability[j] < rand_float; j++ {
+			}
+			copy(population[i],next_generation_candidates[int(fitnesses[1][j])])
+		}
+
+		// always let the top individual pass through
+		copy(population[0],next_generation_candidates[int(fitnesses[1][0])])
+	}
+
+	return population[0], max_fitness
+}
+
 
 // NOTE: functions below that are not found above can be found in sample_functions.go
 func main() {
@@ -127,12 +211,15 @@ func main() {
 	// run the problem on our "simple" function, where we try take an array of values and try to set them to
 	// values between 1 and 10, in order to maximize an objective function sum(x_i*i)
 	fmt.Println("\nRUN ON SIMPLE FUNCTION")
-	best_solution, highest_score := beam_search(2,simple_evaluation,simple_create_random_start,simple_get_neighbors)
+	best_solution, highest_score := beam_search(5,simple_evaluation,simple_create_random_start,simple_get_neighbors)
 	fmt.Println("beam search results", best_solution, highest_score)
+	best_solution, highest_score = stochastic_beam_search(2,simple_evaluation,simple_create_random_start,simple_get_neighbors)
+	fmt.Println("stochastic beam search results", best_solution, highest_score)
 
 	fmt.Println("\nRUN ON TSP")
 	tsp_setup_data()
-	best_solution, highest_score = beam_search(100,tsp_evaluation,tsp_create_random_start,tsp_get_neighbors)
+	best_solution, highest_score = beam_search(10,tsp_evaluation,tsp_create_random_start,tsp_get_neighbors)
 	fmt.Println("beam search results", best_solution, -highest_score)
-	
+	best_solution, highest_score = stochastic_beam_search(10,tsp_evaluation,tsp_create_random_start,tsp_get_neighbors)
+	fmt.Println("stochastic beam search results", best_solution, -highest_score)
 }
