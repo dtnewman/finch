0e94bae295ce66fb554a5252c1568d31659da83e
Daniel Newman <dtnewman@gmail.com>
2014-04-23 13:15:45 -0400

    formatted code to go style guidelines

--- src/beam_search/beam_search.go
+++ src/beam_search/beam_search.go
@@ -5,15 +5,16 @@ import "time"
 import "math/rand"
 import "math"
 
-
 // the recursive loop called by qsort_2d below
-func qsort_inner(a []float64,  b []int) ([]float64, []int) {
-	if len(a) < 2 { return a,b }
+func qsort_inner(a []float64, b []int) ([]float64, []int) {
+	if len(a) < 2 {
+		return a, b
+	}
 
-	left, right := 0, len(a) - 1
+	left, right := 0, len(a)-1
 
 	// Pick a pivot
-	pivotIndex := rand.Int() % len(a) 
+	pivotIndex := rand.Int() % len(a)
 
 	// Move the pivot to the right
 	a[pivotIndex], a[right] = a[right], a[pivotIndex]
@@ -21,36 +22,34 @@ func qsort_inner(a []float64,  b []int) ([]float64, []int) {
 
 	// Pile elements smaller than the pivot on the left
 	for i := range a {
-	    if a[i] < a[right] {
-	    	a[i], a[left] = a[left], a[i]
-	    	b[i], b[left] = b[left], b[i]
-	    	left++
-	    }
+		if a[i] < a[right] {
+			a[i], a[left] = a[left], a[i]
+			b[i], b[left] = b[left], b[i]
+			left++
+		}
 	}
 	// Place the pivot after the last smaller element
 	a[left], a[right] = a[right], a[left]
 	b[left], b[right] = b[right], b[left]
 
 	// Go down the rabbit hole
-	qsort_inner(a[:left],b[:left])
-	qsort_inner(a[left + 1:], b[left + 1:])
-	
-	return a,b
+	qsort_inner(a[:left], b[:left])
+	qsort_inner(a[left+1:], b[left+1:])
+
+	return a, b
 }
 
-
-
 // takes in a 2d array and an index of the row to sort by
-// and returns a 2d array with all rows sorted by the 
+// and returns a 2d array with all rows sorted by the
 // index row. Assumes that the input array is square (nxn)
 func qsort_2d(a_input [][]float64, idx int, ascend_or_desc string) [][]float64 {
-	
+
 	// copy a_input into a
-	a := make([][]float64,len(a_input))
+	a := make([][]float64, len(a_input))
 
 	for i := range a {
 		a[i] = make([]float64, len(a_input[i]))
-		copy(a[i],a_input[i])
+		copy(a[i], a_input[i])
 	}
 
 	// throw error message if ascend_or_desc is not set right
@@ -58,27 +57,27 @@ func qsort_2d(a_input [][]float64, idx int, ascend_or_desc string) [][]float64 {
 	// by -1 and then sort that ascending
 	if ascend_or_desc == "ascending" {
 	} else if ascend_or_desc == "descending" {
-		for i:=0; i < len(a[idx]); i++ {
+		for i := 0; i < len(a[idx]); i++ {
 			a[idx][i] *= -1
-		} 
+		}
 	} else {
 		fmt.Println("ERROR: ascend_or_desc in qsort_2d function must have value of 'ascending' or 'descending'")
 	}
 
 	b := make([]int, len(a[idx]))
-	for i:=0; i < len(a[idx]); i++ {
+	for i := 0; i < len(a[idx]); i++ {
 		b[i] = i
 	}
 
 	// sort by the sorting row
-	_,order := qsort_inner(a[idx],b)
+	_, order := qsort_inner(a[idx], b)
 
 	//sort all other rows
-	for i:=0; i < len(a); i++ {
+	for i := 0; i < len(a); i++ {
 		if i != idx {
-			temp := make([]float64,len(a[i]))
-			copy(temp,a[i])
-			for j:=0; j < len(order); j++ {
+			temp := make([]float64, len(a[i]))
+			copy(temp, a[i])
+			for j := 0; j < len(order); j++ {
 				a[i][j] = temp[order[j]]
 			}
 		}
@@ -86,33 +85,33 @@ func qsort_2d(a_input [][]float64, idx int, ascend_or_desc string) [][]float64 {
 
 	// revert a[idx] row back to original values
 	if ascend_or_desc == "descending" {
-		for i:=0; i < len(a[idx]); i++ {
+		for i := 0; i < len(a[idx]); i++ {
 			a[idx][i] *= -1
-		} 
+		}
 	}
 	return a
 }
 
-// This is my implementation of beam search. It generates a random starting population of size num_beams. 
+// This is my implementation of beam search. It generates a random starting population of size num_beams.
 // Then, at every step, it looks at the neighbors of all members of the population and then finds the top
 // neighbors to create a new population of size num_beams. This keeps going until the max score is no longer
 // improving at which point it stops and returns the best solution.
-func beam_search(num_beams int, evaluate func([]int) float64, 
-				create_random func() []int, get_neighbors func([]int) [][]int)([]int, float64) { 
+func beam_search(num_beams int, evaluate func([]int) float64,
+	create_random func() []int, get_neighbors func([]int) [][]int) ([]int, float64) {
 	// decalre variables
 	var next_generation_candidates [][]int
 	var neighbors [][]int
 	var max_fitness float64 = math.Inf(-1)
 
-	// create a population of num_beams random starting points 
-	population := make([][]int,0)
-	for i:=0; i < num_beams; i++ {
+	// create a population of num_beams random starting points
+	population := make([][]int, 0)
+	for i := 0; i < num_beams; i++ {
 		population = append(population, create_random())
 	}
 	for {
 		// get all the neighbors for all beams and put them into next_generation_candidates
-		next_generation_candidates = make([][]int,0)
-		for i:=0; i < num_beams; i++ {
+		next_generation_candidates = make([][]int, 0)
+		for i := 0; i < num_beams; i++ {
 			neighbors = get_neighbors(population[i])
 			for _, value := range neighbors {
 				next_generation_candidates = append(next_generation_candidates, value)
@@ -120,9 +119,9 @@ func beam_search(num_beams int, evaluate func([]int) float64,
 		}
 
 		// evaluate all the solutions and store values in fitness array
-		fitnesses := make([][]float64,0)
-		fitnesses = append(fitnesses,make([]float64,len(next_generation_candidates)))
-		fitnesses = append(fitnesses,make([]float64,len(next_generation_candidates)))
+		fitnesses := make([][]float64, 0)
+		fitnesses = append(fitnesses, make([]float64, len(next_generation_candidates)))
+		fitnesses = append(fitnesses, make([]float64, len(next_generation_candidates)))
 
 		for i, _ := range fitnesses[0] {
 			fitnesses[0][i] = evaluate(next_generation_candidates[i])
@@ -130,15 +129,15 @@ func beam_search(num_beams int, evaluate func([]int) float64,
 		}
 
 		// sort the fitness array by value in the first row, which holds fitness scores
-		fitnesses = qsort_2d(fitnesses,0, "descending")
+		fitnesses = qsort_2d(fitnesses, 0, "descending")
 		// Stop the loop if the highest fitness is not greater than max_fitness
-		if fitnesses[0][0] <= max_fitness{
+		if fitnesses[0][0] <= max_fitness {
 			break
-		} 
+		}
 
 		// Now fill up the next generation
 		for i := 0; i < num_beams; i++ {
-			copy(population[i],next_generation_candidates[int(fitnesses[1][i])])
+			copy(population[i], next_generation_candidates[int(fitnesses[1][i])])
 		}
 
 		// Set max_fitness to (-1) times the first element in the first row of fitnesses
@@ -149,36 +148,36 @@ func beam_search(num_beams int, evaluate func([]int) float64,
 	return population[0], max_fitness
 }
 
-// This is my implementation of stochastic beam search. It generates a random starting population of size num_beams. 
+// This is my implementation of stochastic beam search. It generates a random starting population of size num_beams.
 // Then, at every step, it looks at the neighbors of all members of the population and then finds the top
 // neighbors to create a new population of size num_beams. It then selects a new population that is the size of num_beams
-// by selecting individuals with the highest fitness individuals more likely to be picked. This keeps going until the 
+// by selecting individuals with the highest fitness individuals more likely to be picked. This keeps going until the
 // max score sees no improvement after max_iterations_no_improvement rounds at which point it stops and returns the
 // best solution.
-func stochastic_beam_search(num_beams int, max_iterations_no_improvement int, evaluate func([]int) float64, 
-				create_random func() []int, get_neighbors func([]int) [][]int)([]int, float64) { 
+func stochastic_beam_search(num_beams int, max_iterations_no_improvement int, evaluate func([]int) float64,
+	create_random func() []int, get_neighbors func([]int) [][]int) ([]int, float64) {
 	// decalre variables
 	var next_generation_candidates [][]int
 	var neighbors [][]int
-	var max_fitness float64 = math.Inf(-1) // will hold max fitness in each generation
+	var max_fitness float64 = math.Inf(-1)      // will hold max fitness in each generation
 	var best_fitness_yet float64 = math.Inf(-1) // will hold highest fitness yet seen
 	var sum_fitnesses float64
 	var rand_float float64
 	var cum_probability float64
 	var num_iterations_no_improvement int = 0
 
-	best_candidate_yet := make([]int,0) // will hold solution with highest fitness yet seen
+	best_candidate_yet := make([]int, 0) // will hold solution with highest fitness yet seen
 
-	// create a population of num_beams random starting points 
-	population := make([][]int,0)
-	for i:=0; i < num_beams; i++ {
+	// create a population of num_beams random starting points
+	population := make([][]int, 0)
+	for i := 0; i < num_beams; i++ {
 		population = append(population, create_random())
 	}
-	for {// num_iterations := 0;num_iterations < 100 ; num_iterations++{
+	for { // num_iterations := 0;num_iterations < 100 ; num_iterations++{
 		//fmt.Println(num_iterations)
 		// get all the neighbors for all beams and put them into next_generation_candidates
-		next_generation_candidates = make([][]int,0)
-		for i:=0; i < num_beams; i++ {
+		next_generation_candidates = make([][]int, 0)
+		for i := 0; i < num_beams; i++ {
 			neighbors = get_neighbors(population[i])
 			for _, value := range neighbors {
 				next_generation_candidates = append(next_generation_candidates, value)
@@ -186,9 +185,9 @@ func stochastic_beam_search(num_beams int, max_iterations_no_improvement int, ev
 		}
 
 		// evaluate all the solutions and store values in fitness array
-		fitnesses := make([][]float64,0)
-		fitnesses = append(fitnesses,make([]float64,len(next_generation_candidates)))
-		fitnesses = append(fitnesses,make([]float64,len(next_generation_candidates)))
+		fitnesses := make([][]float64, 0)
+		fitnesses = append(fitnesses, make([]float64, len(next_generation_candidates)))
+		fitnesses = append(fitnesses, make([]float64, len(next_generation_candidates)))
 
 		for i, _ := range fitnesses[0] {
 			// negate the evaluations, so that it sorts from largest to smallest
@@ -197,8 +196,8 @@ func stochastic_beam_search(num_beams int, max_iterations_no_improvement int, ev
 		}
 
 		// sort the fitness array by value in the first row, which holds fitness scores
-		fitnesses = qsort_2d(fitnesses,0,"descending")
-		
+		fitnesses = qsort_2d(fitnesses, 0, "descending")
+
 		// Stop the loop if the highest fitness is not greater than max_fitness
 		//fmt.Println(fitnesses[0][0])
 		if fitnesses[0][0] <= best_fitness_yet {
@@ -210,49 +209,44 @@ func stochastic_beam_search(num_beams int, max_iterations_no_improvement int, ev
 			break
 		}
 
-
 		// we're gonna pick based on order, with items first getting higher probability. First
-		sum_fitnesses =  float64(len(fitnesses[0])*(len(fitnesses[0])+1))/2.0
+		sum_fitnesses = float64(len(fitnesses[0])*(len(fitnesses[0])+1)) / 2.0
 
-		selection_probability := make([]float64,len(fitnesses[0]))
+		selection_probability := make([]float64, len(fitnesses[0]))
 		cum_probability = 0.0
 
 		for i, _ := range fitnesses[0] {
-			selection_probability[i] = cum_probability+float64(len(fitnesses[0])-i)/sum_fitnesses
-			cum_probability += float64(len(fitnesses[0])-i)/sum_fitnesses
+			selection_probability[i] = cum_probability + float64(len(fitnesses[0])-i)/sum_fitnesses
+			cum_probability += float64(len(fitnesses[0])-i) / sum_fitnesses
 		}
 		//cum_probability = math.Sqrt(cum_probability)
-		
+
 		// Set max_fitness to (-1) times the first element in the first row of fitnesses
 		// since that indicates the leading value
 		max_fitness = fitnesses[0][0]
 
 		if max_fitness > best_fitness_yet {
 			best_fitness_yet = max_fitness
-			best_candidate_yet = make([]int,len(next_generation_candidates[int(fitnesses[1][0])]))
-			copy(best_candidate_yet,next_generation_candidates[int(fitnesses[1][0])])
+			best_candidate_yet = make([]int, len(next_generation_candidates[int(fitnesses[1][0])]))
+			copy(best_candidate_yet, next_generation_candidates[int(fitnesses[1][0])])
 		}
-
 
 		// Now fill up the next generation based on probabilites that are determined from
 		// position
 		for i := 1; i < num_beams; i++ {
 			rand_float = rand.Float64()
 			j := 0
-			for j=0; selection_probability[j] < rand_float; j++ {
+			for j = 0; selection_probability[j] < rand_float; j++ {
 			}
-			copy(population[i],next_generation_candidates[int(fitnesses[1][j])])
+			copy(population[i], next_generation_candidates[int(fitnesses[1][j])])
 		}
 
 		// always let the top individual pass through
-		copy(population[0],next_generation_candidates[int(fitnesses[1][0])])
+		copy(population[0], next_generation_candidates[int(fitnesses[1][0])])
 	}
 
 	return best_candidate_yet, best_fitness_yet
 }
-
-
-
 
 // NOTE: functions below that are not found above can be found in sample_functions.go
 func main() {
@@ -264,15 +258,14 @@ func main() {
 	fmt.Println("beam search results", best_solution, highest_score)
 	best_solution, highest_score = stochastic_beam_search(2,simple_evaluation,simple_create_random_start,simple_get_neighbors)
 	fmt.Println("stochastic beam search results", best_solution, highest_score)
-*/
+	*/
 	fmt.Println("\nRUN ON TSP")
 	tsp_setup_data()
-	best_solution, highest_score := beam_search(50,tsp_evaluation,tsp_create_random_start,tsp_get_neighbors)
+	best_solution, highest_score := beam_search(50, tsp_evaluation, tsp_create_random_start, tsp_get_neighbors)
 	fmt.Println("beam search results", best_solution, -highest_score)
 	plotTSP(best_solution, "tsp_beam_search.png")
-	best_solution, highest_score = stochastic_beam_search(50,1000,tsp_evaluation,tsp_create_random_start,tsp_get_neighbors)
+	best_solution, highest_score = stochastic_beam_search(50, 1000, tsp_evaluation, tsp_create_random_start, tsp_get_neighbors)
 	fmt.Println("stochastic beam search results", best_solution, -highest_score)
 	plotTSP(best_solution, "tsp_stochastic_beam_search.png")
-
 
 }
--- src/tabu/tabu.go
+++ src/tabu/tabu.go
@@ -1,21 +1,20 @@
 package main
 
-
 import "fmt"
 import "time"
 import "math/rand"
 import "math"
 
-
-
 // the recursive loop called by qsort_2d below
-func qsort_inner(a []float64,  b []int) ([]float64, []int) {
-	if len(a) < 2 { return a,b }
+func qsort_inner(a []float64, b []int) ([]float64, []int) {
+	if len(a) < 2 {
+		return a, b
+	}
 
-	left, right := 0, len(a) - 1
+	left, right := 0, len(a)-1
 
 	// Pick a pivot
-	pivotIndex := rand.Int() % len(a) 
+	pivotIndex := rand.Int() % len(a)
 
 	// Move the pivot to the right
 	a[pivotIndex], a[right] = a[right], a[pivotIndex]
@@ -23,36 +22,34 @@ func qsort_inner(a []float64,  b []int) ([]float64, []int) {
 
 	// Pile elements smaller than the pivot on the left
 	for i := range a {
-	    if a[i] < a[right] {
-	    	a[i], a[left] = a[left], a[i]
-	    	b[i], b[left] = b[left], b[i]
-	    	left++
-	    }
+		if a[i] < a[right] {
+			a[i], a[left] = a[left], a[i]
+			b[i], b[left] = b[left], b[i]
+			left++
+		}
 	}
 	// Place the pivot after the last smaller element
 	a[left], a[right] = a[right], a[left]
 	b[left], b[right] = b[right], b[left]
 
 	// Go down the rabbit hole
-	qsort_inner(a[:left],b[:left])
-	qsort_inner(a[left + 1:], b[left + 1:])
-	
-	return a,b
+	qsort_inner(a[:left], b[:left])
+	qsort_inner(a[left+1:], b[left+1:])
+
+	return a, b
 }
 
-
-
 // takes in a 2d array and an index of the row to sort by
-// and returns a 2d array with all rows sorted by the 
+// and returns a 2d array with all rows sorted by the
 // index row. Assumes that the input array is square (nxn)
 func qsort_2d(a_input [][]float64, idx int, ascend_or_desc string) [][]float64 {
-	
+
 	// copy a_input into a
-	a := make([][]float64,len(a_input))
+	a := make([][]float64, len(a_input))
 
 	for i := range a {
 		a[i] = make([]float64, len(a_input[i]))
-		copy(a[i],a_input[i])
+		copy(a[i], a_input[i])
 	}
 
 	// throw error message if ascend_or_desc is not set right
@@ -60,27 +57,27 @@ func qsort_2d(a_input [][]float64, idx int, ascend_or_desc string) [][]float64 {
 	// by -1 and then sort that ascending
 	if ascend_or_desc == "ascending" {
 	} else if ascend_or_desc == "descending" {
-		for i:=0; i < len(a[idx]); i++ {
+		for i := 0; i < len(a[idx]); i++ {
 			a[idx][i] *= -1
-		} 
+		}
 	} else {
 		fmt.Println("ERROR: ascend_or_desc in qsort_2d function must have value of 'ascending' or 'descending'")
 	}
 
 	b := make([]int, len(a[idx]))
-	for i:=0; i < len(a[idx]); i++ {
+	for i := 0; i < len(a[idx]); i++ {
 		b[i] = i
 	}
 
 	// sort by the sorting row
-	_,order := qsort_inner(a[idx],b)
+	_, order := qsort_inner(a[idx], b)
 
 	//sort all other rows
-	for i:=0; i < len(a); i++ {
+	for i := 0; i < len(a); i++ {
 		if i != idx {
-			temp := make([]float64,len(a[i]))
-			copy(temp,a[i])
-			for j:=0; j < len(order); j++ {
+			temp := make([]float64, len(a[i]))
+			copy(temp, a[i])
+			for j := 0; j < len(order); j++ {
 				a[i][j] = temp[order[j]]
 			}
 		}
@@ -88,24 +85,24 @@ func qsort_2d(a_input [][]float64, idx int, ascend_or_desc string) [][]float64 {
 
 	// revert a[idx] row back to original values
 	if ascend_or_desc == "descending" {
-		for i:=0; i < len(a[idx]); i++ {
+		for i := 0; i < len(a[idx]); i++ {
 			a[idx][i] *= -1
-		} 
+		}
 	}
 	return a
 }
 
 // check if two slices (of ints) are equal
 func IntsEquals(a, b []int) bool {
-    if len(a) != len(b) {
-        return false
-    }
-    for i, v := range a {
-        if v != b[i] {
-            return false
-        }
-    }
-    return true
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
 }
 
 // check if a candidate solution is in the tabu list
@@ -118,31 +115,28 @@ func inTabuList(candidate []int, tabuList [][]int) bool {
 	return false
 }
 
+func tabu_search(initial_solution []int, max_num_neighbors int, tabu_list_max_size int, evaluate func([]int) float64,
+	get_neighbors func([]int) [][]int) ([]int, float64) {
+	current_solution := make([]int, len(initial_solution))
+	copy(current_solution, initial_solution)
 
-func tabu_search(initial_solution []int, max_num_neighbors int, tabu_list_max_size int, evaluate func([]int) float64, 
-				get_neighbors func([]int) [][]int) ([]int, float64){
-	current_solution := make([]int,len(initial_solution))
-	copy(current_solution,initial_solution)
-
-
-	var max_fitness float64 = math.Inf(-1) // will hold max fitness in each generation
+	var max_fitness float64 = math.Inf(-1)      // will hold max fitness in each generation
 	var best_fitness_yet float64 = math.Inf(-1) // will hold highest fitness yet seen
-	best_candidate_yet := make([]int,0) // will hold solution with highest fitness yet seen
-	tabu_list := make([][]int,0)
+	best_candidate_yet := make([]int, 0)        // will hold solution with highest fitness yet seen
+	tabu_list := make([][]int, 0)
 
 	num_iterations_no_improvement := 0
 	max_iterations_no_improvement := 100
-
 
 	for num_iterations_no_improvement < max_iterations_no_improvement {
 		//fmt.Println("ITERATION:", 5001-stop_condition)
 		neighbors := get_neighbors(current_solution)
 		//fmt.Println("Num neighbors", len(neighbors))
-	    
+
 		// evaluate all the solutions and store values in fitness array
-		fitnesses := make([][]float64,0)
-		fitnesses = append(fitnesses,make([]float64,len(neighbors)))
-		fitnesses = append(fitnesses,make([]float64,len(neighbors)))
+		fitnesses := make([][]float64, 0)
+		fitnesses = append(fitnesses, make([]float64, len(neighbors)))
+		fitnesses = append(fitnesses, make([]float64, len(neighbors)))
 
 		for i, _ := range fitnesses[0] {
 			fitnesses[0][i] = evaluate(neighbors[i])
@@ -150,7 +144,7 @@ func tabu_search(initial_solution []int, max_num_neighbors int, tabu_list_max_si
 		}
 
 		// sort the fitness array by value in the first row, which holds fitness scores
-		fitnesses = qsort_2d(fitnesses,0, "descending")
+		fitnesses = qsort_2d(fitnesses, 0, "descending")
 
 		fitnesses[0] = fitnesses[0][:max_num_neighbors]
 		fitnesses[1] = fitnesses[1][:max_num_neighbors]
@@ -161,8 +155,8 @@ func tabu_search(initial_solution []int, max_num_neighbors int, tabu_list_max_si
 		// update best_fitness_yet and best_candidate_yet
 		if max_fitness > best_fitness_yet {
 			best_fitness_yet = max_fitness
-			best_candidate_yet = make([]int,len(neighbors[int(fitnesses[1][0])]))
-			copy(best_candidate_yet,neighbors[int(fitnesses[1][0])])
+			best_candidate_yet = make([]int, len(neighbors[int(fitnesses[1][0])]))
+			copy(best_candidate_yet, neighbors[int(fitnesses[1][0])])
 			num_iterations_no_improvement = 0
 		}
 
@@ -170,7 +164,7 @@ func tabu_search(initial_solution []int, max_num_neighbors int, tabu_list_max_si
 		// until a candidate that is not in the tabu list is found. If none are found
 		// then break
 		next_candidate_idx := 0
-		for inTabuList(neighbors[int(fitnesses[1][next_candidate_idx])],tabu_list) {
+		for inTabuList(neighbors[int(fitnesses[1][next_candidate_idx])], tabu_list) {
 			next_candidate_idx += 1
 			// if next_candidate_idx == num_neighbors means that all of the neighbors
 			// are in the tabu list
@@ -182,49 +176,46 @@ func tabu_search(initial_solution []int, max_num_neighbors int, tabu_list_max_si
 		//fmt.Println("next_candidate_idx", next_candidate_idx)
 
 		tabu_list = append(tabu_list, neighbors[int(fitnesses[1][next_candidate_idx])])
-		if len(tabu_list)>tabu_list_max_size {
+		if len(tabu_list) > tabu_list_max_size {
 			tabu_list[0] = nil
 			tabu_list = tabu_list[1:]
 		}
-
 
 		//fmt.Println("tabu list size:", len(tabu_list))
 		current_solution = neighbors[int(fitnesses[1][next_candidate_idx])]
 
 		//fmt.Println("best fitness:", max_fitness)
 
-
-	    
 	}
 	return best_candidate_yet, best_fitness_yet
 }
 
-func tabu_search_go_routine(current_solution [] int, max_num_neighbors int, tabu_list_max_size int, evaluate func([]int) float64, get_neighbors func([]int) [][]int, ch1 chan<- []int, ch2 chan <- float64) {
+func tabu_search_go_routine(current_solution []int, max_num_neighbors int, tabu_list_max_size int, evaluate func([]int) float64, get_neighbors func([]int) [][]int, ch1 chan<- []int, ch2 chan<- float64) {
 	best_solution, highest_score := tabu_search(current_solution, max_num_neighbors, tabu_list_max_size, evaluate, get_neighbors)
 	ch1 <- best_solution
 	ch2 <- highest_score
 }
 
-func random_restart_tabu_search(num_restarts int, max_num_neighbors int, tabu_list_max_size int,evaluate func([]int) float64, create_random func() []int, get_neighbors func([]int) [][]int)([]int, float64) {
+func random_restart_tabu_search(num_restarts int, max_num_neighbors int, tabu_list_max_size int, evaluate func([]int) float64, create_random func() []int, get_neighbors func([]int) [][]int) ([]int, float64) {
 	var highest_score float64
 	highest_score = math.Inf(-1)
 	ch1 := make(chan []int)
 	ch2 := make(chan float64)
 
 	for i := 0; i < num_restarts; i++ {
-		go tabu_search_go_routine(create_random(),max_num_neighbors, tabu_list_max_size,evaluate, get_neighbors, ch1, ch2)
+		go tabu_search_go_routine(create_random(), max_num_neighbors, tabu_list_max_size, evaluate, get_neighbors, ch1, ch2)
 	}
 
-	var score float64;
+	var score float64
 	var best_solution []int
 
 	for i := 0; i < num_restarts; i++ {
-        best_solution = <-ch1
-        score = <-ch2
-        if score > highest_score {
-        	highest_score = score
-        }
-    }
+		best_solution = <-ch1
+		score = <-ch2
+		if score > highest_score {
+			highest_score = score
+		}
+	}
 
 	return best_solution, highest_score
 }
@@ -234,31 +225,29 @@ func main() {
 	rand.Seed(time.Now().Unix())
 	rand.Seed(8)
 
+	// Run on a travelling salesman problem with cities in the file tsp_data.csv (40 cities)
+	fmt.Println("RUN ON TSP")
+	tsp_setup_data()
 
-    // Run on a travelling salesman problem with cities in the file tsp_data.csv (40 cities)
-    fmt.Println("RUN ON TSP")
-    tsp_setup_data()
+	for i := 0; i < 3; i++ {
+		p := tsp_create_random_start()
+		fmt.Println("Initial distance:", -tsp_evaluation(p))
+		_, highest_score := hill_climb(p, tsp_evaluation, tsp_get_neighbors)
+		fmt.Println("regular hill climb:", -highest_score)
+		_, highest_score = tabu_search(p, 4, 2, tsp_evaluation, tsp_get_neighbors)
+		fmt.Println("tabu search, max_num_neighbors=4, tabu_list_max_size=2:", -highest_score, "")
+		_, highest_score = tabu_search(p, 40, 20, tsp_evaluation, tsp_get_neighbors)
+		fmt.Println("tabu search, max_num_neighbors=40, tabu_list_max_size=20:", -highest_score, "")
+		_, highest_score = tabu_search(p, 400, 200, tsp_evaluation, tsp_get_neighbors)
+		fmt.Println("tabu search, max_num_neighbors=400, tabu_list_max_size=200:", -highest_score, "\n")
+	}
 
-    for i := 0; i < 3; i++ {
-    	p := tsp_create_random_start()
-	    fmt.Println("Initial distance:", -tsp_evaluation(p))
-	    _, highest_score := hill_climb(p,tsp_evaluation,tsp_get_neighbors)
-	    fmt.Println("regular hill climb:", -highest_score)
-	    _, highest_score = tabu_search(p,4,2,tsp_evaluation,tsp_get_neighbors)
-	    fmt.Println("tabu search, max_num_neighbors=4, tabu_list_max_size=2:", -highest_score, "")
-	    _, highest_score = tabu_search(p,40,20,tsp_evaluation,tsp_get_neighbors)
-	    fmt.Println("tabu search, max_num_neighbors=40, tabu_list_max_size=20:", -highest_score, "")
-	    _, highest_score = tabu_search(p,400,200,tsp_evaluation,tsp_get_neighbors)
-	    fmt.Println("tabu search, max_num_neighbors=400, tabu_list_max_size=200:", -highest_score, "\n")
-    }
+	best_solution, highest_score := random_restart_tabu_search(50, 20, 10, tsp_evaluation, tsp_create_random_start, tsp_get_neighbors)
+	fmt.Println("Optimized distance (random restart tabu search):", best_solution, -highest_score)
 
+	plotTSP(best_solution, "tabu_search.png")
 
-    best_solution, highest_score := random_restart_tabu_search(50,20,10,tsp_evaluation,tsp_create_random_start,tsp_get_neighbors)
-    fmt.Println("Optimized distance (random restart tabu search):", best_solution, -highest_score)
-
-    plotTSP(best_solution, "tabu_search.png")
-    
-    random_solution := tsp_create_random_start()
-    plotTSP(random_solution, "random_solution.png")
+	random_solution := tsp_create_random_start()
+	plotTSP(random_solution, "random_solution.png")
 
 }
