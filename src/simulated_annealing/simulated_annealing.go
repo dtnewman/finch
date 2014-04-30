84a46f582b6b0213e7bb0876e84d9bc3e29f710d
Daniel Newman <dtnewman@gmail.com>
2014-04-30 13:45:02 -0400

    updated formatting

--- src/simulated_annealing/simulated_annealing.go
+++ src/simulated_annealing/simulated_annealing.go
@@ -8,15 +8,14 @@ import "strconv"
 
 // Uses the make_change function to make num_changes to the current solution and then returns the
 // solution with the random changes made
-func make_changes(current_solution []int, make_change func([]int) []int, num_changes int)([] int) {
+func make_changes(current_solution []int, make_change func([]int) []int, num_changes int) []int {
 	new_solution := make([]int, len(current_solution))
 	copy(new_solution, current_solution)
-	for i:=0;i<num_changes;i++ {
+	for i := 0; i < num_changes; i++ {
 		new_solution = make_change(new_solution)
 	}
 	return new_solution
 }
-
 
 /*  A simulated annealing algorithm that makes random changes in search of a global maximum
 *	@param[in] initial_solution The starting point
@@ -27,12 +26,12 @@ func make_changes(current_solution []int, make_change func([]int) []int, num_cha
 	@param[in] init_temp The starting temperature for the algorithm
 	@param[in] thermostat The thermostat level for determine how quickly to decrease temperature
 	@param[in] itol The max number of iterations. Once this is reached, the algorithm stops
-	@param[in] reannealing The number of rounds before trying reannealing 
+	@param[in] reannealing The number of rounds before trying reannealing
 *	@return Returns a slice containing the best solution as well as a float64 with
 *	that solution's evaluated score
- */
+*/
 func simulated_annealing(initial_solution []int, evaluate func([]int) float64, make_change func([]int) []int,
-						 init_temp float64, thermostat float64, itol int, reannealing int)([]int, float64) {
+	init_temp float64, thermostat float64, itol int, reannealing int) ([]int, float64) {
 
 	// setup all the necessary variables
 	current_solution := make([]int, len(initial_solution))
@@ -48,49 +47,46 @@ func simulated_annealing(initial_solution []int, evaluate func([]int) float64, m
 	highest_score := evaluate(current_solution)
 	prev_score = highest_score
 
-	temperature := init_temp 
+	temperature := init_temp
 	L := 0
 	num_acceptances := 0
-
 
 	for i := 0; i < itol; i++ {
 		L = int(math.Sqrt(temperature))
 		proposed_solution = make_changes(current_solution, make_change, L)
 		new_score = evaluate(proposed_solution)
-		delta_score = new_score-prev_score
+		delta_score = new_score - prev_score
 
 		// If the proposed solution is better than the current solution, then
 		// accept it. Otherwise, accept with probability E^(delta_score/temperature)
-		if (new_score > prev_score) {
+		if new_score > prev_score {
 			copy(current_solution, proposed_solution)
-			prev_score = new_score 
-			if (new_score > highest_score) {
+			prev_score = new_score
+			if new_score > highest_score {
 				highest_score = new_score
 				copy(return_solution, proposed_solution)
 			}
 			num_acceptances++
-		} else if (rand.Float64() < math.Exp(delta_score/temperature)) {
+		} else if rand.Float64() < math.Exp(delta_score/temperature) {
 			copy(current_solution, proposed_solution)
-			prev_score = new_score 
+			prev_score = new_score
 			num_acceptances++
-		} 
+		}
 
 		// adjust temperature after the number of acceptances determined by reannealing parameter
-		if (num_acceptances % reannealing == 0) {
+		if num_acceptances%reannealing == 0 {
 			temperature = thermostat * temperature
 			// If the temperature goes below 1, reset it to the initial temperature
-			if (temperature < 1){
+			if temperature < 1 {
 				temperature = init_temp
 			}
 		}
 
-
-	} 
+	}
 
 	// Return the best solution and the highest score
 	return return_solution, highest_score
 }
-
 
 // NOTE: functions below that are not found above can be found in sample_functions.go
 func main() {
@@ -102,9 +98,9 @@ func main() {
 	p := []int{2, 3, 5, 4, 1, 6}
 	fmt.Println("p", p)
 
-	best_solution, highest_score := simulated_annealing(p, simple_evaluation,simple_make_change,10,0.9,1000,10)
+	best_solution, highest_score := simulated_annealing(p, simple_evaluation, simple_make_change, 10, 0.9, 1000, 10)
 	fmt.Println("Simulated annealing results", best_solution, highest_score)
-	
+
 	// Run on a travelling salesman problem with cities in the file tsp_data.csv (40 cities)
 	fmt.Println("RUN ON TSP")
 	tsp_setup_data()
@@ -122,10 +118,10 @@ func main() {
 	num_runs := 10
 
 	fmt.Println("Initial distance:", -tsp_evaluation(p2))
-	total := 0.0;
-	for i:=0;i<num_runs;i++ {
+	total := 0.0
+	for i := 0; i < num_runs; i++ {
 		p2 = tsp_create_random_start()
-		best_solution, highest_score = simulated_annealing(p2, tsp_evaluation,tsp_make_change,init_temp,thermostat,iterations,reannealing)
+		best_solution, highest_score = simulated_annealing(p2, tsp_evaluation, tsp_make_change, init_temp, thermostat, iterations, reannealing)
 		fmt.Println(-highest_score)
 		total += highest_score
 		plotTSP(best_solution, "tsp_SA"+strconv.Itoa(i)+".png")
